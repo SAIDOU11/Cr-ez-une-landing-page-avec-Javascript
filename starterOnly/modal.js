b008f7032e876c3709b8cb6426c4897acb47f3d0
Salif Saidou <saidou02100@gmail.com>
2023-02-25 20:31:48 +0100

    email regexp

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -33,4 +33,50 @@ closeBtn[0].addEventListener("click", () => {
   return closeModal();
 });
 
-//
+/* 
+first
+last
+email
+birthdate
+quantity
+*/
+const registForm = document.getElementsByName("reserve");
+console.log(registForm);
+
+const emailAddress = document.getElementById("email");
+console.log(emailAddress);
+
+emailAddress.addEventListener("change", () => {
+  validEmail(this);
+});
+
+const validEmail = (inputEmail) => {
+  console.log(inputEmail["email"].value);
+  //Création de l'expression régulière
+  let emailRegExp = new RegExp(
+    "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$",
+    "g"
+  );
+
+  // //Test de l'expression
+  if (
+    inputEmail["email"].value &&
+    emailRegExp.test(inputEmail["email"].value)
+  ) {
+    alert("EMAIL VALIDE");
+  } else {
+    alert("NON VALIDE");
+  }
+};
+
+// const firstName = document.getElementById("first");
+// firstName.addEventListener("change", () => {
+//   console.log();
+// });
+
+// const lastName = document.getElementById("last");
+// console.log(lastName);
+// const birthDate = document.getElementById("birthdate");
+// console.log(birthDate);
+// const quantity = document.getElementById("quantity");
+// console.log(quantity);
